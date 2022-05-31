f8141ac36dee5abbcc15a1d6015ab6b78e5733f5
Flexberry-man <mail@flexberry.net>
2022-05-31 06:38:20 +0000

    Flexberry code generation

--- src/ember-app/app/controllers/application.js
+++ src/ember-app/app/controllers/application.js
@@ -34,31 +34,31 @@ export default Controller.extend({
             link: 'i-i-s-gor-vodokanal-request-l',
             caption: i18n.t('forms.application.sitemap.gor-vodokanal.i-i-s-gor-vodokanal-request-l.caption'),
             title: i18n.t('forms.application.sitemap.gor-vodokanal.i-i-s-gor-vodokanal-request-l.title'),
-            icon: 'suitcase',
+            icon: 'edit',
             children: null
           }, {
             link: 'i-i-s-gor-vodokanal-request-cause-l',
             caption: i18n.t('forms.application.sitemap.gor-vodokanal.i-i-s-gor-vodokanal-request-cause-l.caption'),
             title: i18n.t('forms.application.sitemap.gor-vodokanal.i-i-s-gor-vodokanal-request-cause-l.title'),
-            icon: 'phone',
+            icon: 'file',
             children: null
           }, {
             link: 'i-i-s-gor-vodokanal-employee-l',
             caption: i18n.t('forms.application.sitemap.gor-vodokanal.i-i-s-gor-vodokanal-employee-l.caption'),
             title: i18n.t('forms.application.sitemap.gor-vodokanal.i-i-s-gor-vodokanal-employee-l.title'),
-            icon: 'book',
+            icon: 'suitcase',
             children: null
           }, {
             link: 'i-i-s-gor-vodokanal-dictionary-l',
             caption: i18n.t('forms.application.sitemap.gor-vodokanal.i-i-s-gor-vodokanal-dictionary-l.caption'),
             title: i18n.t('forms.application.sitemap.gor-vodokanal.i-i-s-gor-vodokanal-dictionary-l.title'),
-            icon: 'edit',
+            icon: 'file',
             children: null
           }, {
             link: 'i-i-s-gor-vodokanal-request-kind-l',
             caption: i18n.t('forms.application.sitemap.gor-vodokanal.i-i-s-gor-vodokanal-request-kind-l.caption'),
             title: i18n.t('forms.application.sitemap.gor-vodokanal.i-i-s-gor-vodokanal-request-kind-l.title'),
-            icon: 'briefcase',
+            icon: 'building',
             children: null
           }]
         }
