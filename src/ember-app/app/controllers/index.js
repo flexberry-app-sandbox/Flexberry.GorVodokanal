cfcf55c810ca2e7993f46fa1bd0f30cc95909278
Flexberry-man <mail@flexberry.net>
2022-05-31 07:03:37 +0000

    Flexberry code generation

--- src/ember-app/app/controllers/index.js
+++ src/ember-app/app/controllers/index.js
@@ -22,37 +22,36 @@ export default Controller.extend({
             link: 'i-i-s-gor-vodokanal-request-type-l',
             caption: i18n.t('forms.application.sitemap.gor-vodokanal.i-i-s-gor-vodokanal-request-type-l.caption'),
             title: i18n.t('forms.application.sitemap.gor-vodokanal.i-i-s-gor-vodokanal-request-type-l.title'),
-            icon: 'archive',
+            icon: 'tags',
             children: null
           }, {
             link: 'i-i-s-gor-vodokanal-request-l',
             caption: i18n.t('forms.application.sitemap.gor-vodokanal.i-i-s-gor-vodokanal-request-l.caption'),
             title: i18n.t('forms.application.sitemap.gor-vodokanal.i-i-s-gor-vodokanal-request-l.title'),
-            icon: 'edit',
+            icon: 'folder open',
             children: null
           }, {
             link: 'i-i-s-gor-vodokanal-request-cause-l',
             caption: i18n.t('forms.application.sitemap.gor-vodokanal.i-i-s-gor-vodokanal-request-cause-l.caption'),
             title: i18n.t('forms.application.sitemap.gor-vodokanal.i-i-s-gor-vodokanal-request-cause-l.title'),
-            icon: 'file',
+            icon: 'calendar',
             children: null
           }, {
             link: 'i-i-s-gor-vodokanal-employee-l',
             caption: i18n.t('forms.application.sitemap.gor-vodokanal.i-i-s-gor-vodokanal-employee-l.caption'),
             title: i18n.t('forms.application.sitemap.gor-vodokanal.i-i-s-gor-vodokanal-employee-l.title'),
-            icon: 'suitcase',
+            icon: 'tasks',
             children: null
           }, {
             link: 'i-i-s-gor-vodokanal-dictionary-l',
             caption: i18n.t('forms.application.sitemap.gor-vodokanal.i-i-s-gor-vodokanal-dictionary-l.caption'),
             title: i18n.t('forms.application.sitemap.gor-vodokanal.i-i-s-gor-vodokanal-dictionary-l.title'),
-            icon: 'file',
+            icon: 'briefcase',
             children: null
           }, {
             link: 'i-i-s-gor-vodokanal-request-kind-l',
             caption: i18n.t('forms.application.sitemap.gor-vodokanal.i-i-s-gor-vodokanal-request-kind-l.caption'),
             title: i18n.t('forms.application.sitemap.gor-vodokanal.i-i-s-gor-vodokanal-request-kind-l.title'),
-            icon: 'building',
             children: null
           }]
         }
